c790127726e5eeea4fdc63f2de5383017a95efeb
Ahmed-abdel-halim <ahmedholim42@gmail.com>
2026-04-04 14:33:51 +0200

    first commit

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,27 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
+// Fix for ngrok + Vercel
+const originalFetch = window.fetch;
+window.fetch = (...args) => {
+  let [resource, config] = args;
+  
+  // 1. تحويل الروابط النسبية لوابط ngrok مباشرة لتجنب مشاكل بروكسي فيرسل
+  if (typeof resource === 'string' && resource.startsWith('/api')) {
+    resource = 'https://nonmetallic-pa-unprojecting.ngrok-free.dev' + resource;
+  }
+  
+  // 2. إضافة هيدر تخطي تحذير ngrok لكل الطلبات
+  const newConfig = { ...config } as RequestInit;
+  newConfig.headers = {
+    ...newConfig.headers,
+    'ngrok-skip-browser-warning': 'true',
+    'Accept': 'application/json'
+  };
+  
+  return originalFetch(resource, newConfig);
+};
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <App />
